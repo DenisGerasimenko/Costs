d4e8898fdbff7b76d1e941fcb68c08d9c465d430
DenisGerasimenko <gerasimenkodenis7@gmail.com>
2022-08-18 12:40:22 +0300

    release v1

---
+++ src/components/Costs/Costs.js
@@ -0,0 +1,33 @@
+import "./Costs.css";
+import Card from "../UI/Card";
+import CostsFilter from "./CostsFilter";
+import React, {useState} from "react";
+import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
+
+
+
+const Costs = (props) => {
+
+    const [selectedYear, setSelectedYear] = useState('2021');
+
+    const addYearHandler = (year) => {
+        setSelectedYear(year);
+    };
+
+    const filteredCosts = props.costs.filter(cost => {
+        return cost.date.getFullYear().toString() === selectedYear;
+    });
+
+    return (
+        <div>
+            <Card className="costs">
+                <CostsFilter onChangeYear={addYearHandler} year={selectedYear}/>
+                <CostsDiagram costs={filteredCosts}/>
+                <CostList costs={filteredCosts}/>
+            </Card>
+        </div>
+    );
+};
+
+export default Costs;
